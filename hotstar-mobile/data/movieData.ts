3cec19dfcbd5e3975826c76fb9c26a8a8e9d818e
StackVishnu <vishnu.rathan2000@gmail.com>
2024-05-21 18:39:23 +0530

    added data folder to store movieData

---
+++ hotstar-mobile/data/movieData.ts
@@ -0,0 +1,14 @@
+export interface MovieData {
+  titleImg: string;
+  bgImg: string;
+  mName: string;
+}
+
+const titleData: MovieData[] = [
+  {
+    titleImg:
+      "https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/7863/1712686147863-t",
+    bgImg: "",
+    mName: "",
+  },
+];
